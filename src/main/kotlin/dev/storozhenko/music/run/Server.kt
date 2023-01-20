de70ecd57249274e53dd9537e777c6efa77d4efb
Yaroslav <yaroslav@storozhenko.dev>
2023-01-20 22:10:25 +0000

    fucking ktor

--- src/main/kotlin/dev/storozhenko/music/run/Server.kt
+++ src/main/kotlin/dev/storozhenko/music/run/Server.kt
@@ -3,36 +3,38 @@ package dev.storozhenko.music.run
 import com.adamratzman.spotify.SpotifyUserAuthorization
 import dev.storozhenko.music.getLogger
 import dev.storozhenko.music.services.SpotifyService
-import io.ktor.application.call
-import io.ktor.response.respondRedirect
-import io.ktor.response.respondText
-import io.ktor.routing.get
-import io.ktor.routing.routing
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.util.getOrFail
+import io.ktor.server.response.respondRedirect
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import io.ktor.server.util.getOrFail
 
 class Server(private val spotifyService: SpotifyService) {
 
-    private val log = getLogger()
 
     fun run() {
-        embeddedServer(Netty, port = 9999) {
-            routing {
-                get("/") {
-                    val url = spotifyService.getAuthUrl()
-                    call.respondRedirect(url, permanent = false)
-                    log.info("Sent redirect to $url")
-                }
-                get("/callback") {
-                    val authCode = call.request.queryParameters.getOrFail("code")
-                    spotifyService.createClient(
-                        SpotifyUserAuthorization(authorizationCode = authCode)
-                    )
-                    log.info("Got callback, client has been created")
-                    call.respondText { "OK" }
-                }
-            }
-        }.start(wait = true)
+        embeddedServer(Netty, port = 9999, module = {module(spotifyService)}).start(wait = true)
+    }
+}
+
+fun Application.module(spotifyService: SpotifyService) {
+    routing {
+        get("/") {
+            val url = spotifyService.getAuthUrl()
+            call.respondRedirect(url, permanent = false)
+            getLogger().info("Sent redirect to $url")
+        }
+        get("/callback") {
+            val authCode = call.request.queryParameters.getOrFail("code")
+            spotifyService.createClient(
+                SpotifyUserAuthorization(authorizationCode = authCode)
+            )
+            getLogger().info("Got callback, client has been created")
+            call.respondText { "OK" }
+        }
     }
 }
